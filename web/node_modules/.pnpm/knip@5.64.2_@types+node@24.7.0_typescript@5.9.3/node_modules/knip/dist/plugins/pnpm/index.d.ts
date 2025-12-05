421799de5de470679436f212dba36b80ae2d7655
Alistair Cunningham <acunningham@integrics.com>
2025-12-05 16:58:08 +0900

    remove web/dist and web/node_modules from git

--- web/node_modules/.pnpm/knip@5.64.2_@types+node@24.7.0_typescript@5.9.3/node_modules/knip/dist/plugins/pnpm/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { IsPluginEnabled } from '../../types/config.js';
-declare const _default: {
-    title: string;
-    isEnabled: IsPluginEnabled;
-    isRootOnly: true;
-    config: string[];
-};
-export default _default;
